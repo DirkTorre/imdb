0cec1f49cf458163646424a2930e560ffa060c91
DirkTorre <dirkvdtorre@live.nl>
2023-11-16 22:35:00 +0100

    dowload_imdb_files now works with: - python 3.12 - pandas 2.x

--- download_imdb_files.py
+++ download_imdb_files.py
@@ -1,22 +1,12 @@
-# downloads all files and convert them to more logical parquet files.
-
-# directors and writers are still not put in the right folder, and i don't know why!!!!
-# I think it happens after title principals starts/crashes
-# update: nothing is wrong before title.principals starts. 
-#            the writers and directors parque files are in the right folder
-
-
-import os
-import re
-import sys
+import pyarrow as pa
+import pandas as pd
+import numpy as np
+import requests
+import shutil
 import time
 import gzip
-import shutil
-import requests
-import numpy as np
-import pandas as pd
-from datetime import date
-from datetime import timedelta
+import sys
+import os
 
 START_TIME = time.time()
 
@@ -36,6 +26,7 @@ FILES_IMDB = {
     "tit_prin" : "title.principals.tsv",
 }
 
+
 FILES_IMDB_PARQ = {
     "tit_bas" : "title_basics.parquet",
     "genres" : "genres.parquet",
@@ -50,19 +41,6 @@ FILES_IMDB_PARQ = {
     'character' : 'character.parquet',
     'job' : 'job.parquet',
 }
-
-FILES_HAND = {
-    "add_seen": "add_movies_seen.txt",
-    "add_unseen": "add_movies_unseen.txt",
-    "add_secop": "add_movies_second_opinion.txt",
-    "raw_status": "raw_status.xlsx"
-}
-
-FILES_GENERATED = {
-    "films_raw": "films_raw.pkl",
-    "films_reading": "films_reading.xlsx"
-}
-
 
 
 def main():
@@ -148,27 +126,28 @@ def main():
             pass
 
 
+# updated
 def setTNconst(df, key, set_index, drop_col):
     # helper function to convert id's to integers
     # tconst
     # We remove the first two tt and convert it to int.
-    # Commands are not chained, baecause this can cause memory issues with big files.
-    df.loc[:,key] = df.loc[:,key].str.slice(2)
-    df.loc[:,key] = df.loc[:,key].astype('Int64')
-    df.loc[:,key] = df.loc[:,key].astype('category')
+    # Commands are not chained, baecause this can cause memory issues with big files in pandas 1.x.
+    df[key] = df[key].str.slice(2)
+    df[key] = df[key].astype(pd.ArrowDtype(pa.uint32()))
+    df[key] = df[key].astype('category')
     if set_index:
         df = df.set_index(key, drop=drop_col)
     return df
 
 
 def convertIds():
+    # load files
     title_basics_path = os.path.join(DOWNLOAD_PATH,FILES_IMDB['tit_bas'])
-    tconst = pd.read_csv(title_basics_path,sep='\t', quotechar='\t', low_memory=False, usecols=['tconst'])
-    tconst = tconst.replace(to_replace = "\\N", value = np.nan)
+    tconst = pd.read_csv(title_basics_path,sep='\t', quotechar='\t',
+                         low_memory=False, usecols=['tconst'], dtype_backend="pyarrow")
 
     name_bas_path = os.path.join(DOWNLOAD_PATH,FILES_IMDB['name_bas'])
-    nconst = pd.read_csv(name_bas_path,sep='\t', low_memory=True, usecols=['nconst'])
-    nconst = nconst.replace(to_replace = "\\N", value = np.nan)
+    nconst = pd.read_csv(name_bas_path,sep='\t', low_memory=True, usecols=['nconst'], dtype_backend="pyarrow")
 
     # make new columns and rename
     tconst['stringid'] = tconst['tconst']
@@ -183,9 +162,9 @@ def convertIds():
     const = setTNconst(pd.concat([tconst, nconst]), 'intid', set_index=False, drop_col=False)
 
     # set types
-    const.loc[:,'stringid'] = const.loc[:,'stringid'].astype('category')
-    const.loc[:,'type'] = const.loc[:,'type'].astype('category')
-    const.loc[:,'intid'] = const.loc[:,'intid'].astype(int)
+    const['stringid'] = const['stringid'].astype('category')
+    const['type'] = const['type'].astype('category')
+    const['intid'] = const['intid'].astype(int)
 
     # set index
     const = const.set_index("stringid", drop=True)
@@ -193,50 +172,58 @@ def convertIds():
     # write to disk
     path = os.path.join(PARQ_PATH, FILES_IMDB_PARQ['const'])
     const.to_parquet(path, engine='pyarrow')
-    
-    # unnecessary, but i give it a try
-    del const
-    del tconst
-    del nconst
+
+
+def convertTitleRate():
+    # read
+    title_rate_path = os.path.join(DOWNLOAD_PATH,FILES_IMDB['tit_rate'])
+    title_rate = pd.read_csv(title_rate_path,sep='\t', quotechar='\t',
+                             low_memory=False, dtype_backend="pyarrow")
+
+    # convert and set tconst as index
+    title_rate = setTNconst(title_rate, 'tconst', set_index=True, drop_col=True)
+
+    # convert averageRating
+    title_rate['averageRating'] = title_rate['averageRating'].astype(pd.ArrowDtype(pa.float32()))
+
+    # convert numVotes
+    title_rate['numVotes'] = title_rate['numVotes'].astype(pd.ArrowDtype(pa.uint32()))
+
+    # write to disk
+    path = os.path.join(PARQ_PATH, FILES_IMDB_PARQ['tit_rate'])
+    title_rate.to_parquet(path, engine='pyarrow')
 
 
 def convertTitleBasics():
     # read
     title_basics_path = os.path.join(DOWNLOAD_PATH,FILES_IMDB['tit_bas'])
-    title_basics = pd.read_csv(title_basics_path,sep='\t', quotechar='\t', low_memory=False)
+    title_basics = pd.read_csv(title_basics_path,sep='\t', quotechar='\t',
+                               low_memory=False, dtype_backend="pyarrow")
     title_basics = title_basics.replace(to_replace = "\\N", value = np.nan)
 
     # convert and set tconst as index
     title_basics = setTNconst(title_basics, 'tconst', set_index=True, drop_col=True)
 
     # convert isAdult
-    # Those movies are checked, and were not adult movies.
-    # movies that are not an 1 or '1' aren't adult movies
-    title_basics.loc[:,"isAdult"] = title_basics.loc[:,"isAdult"].fillna(0).astype('str')
-    title_basics.loc[title_basics.loc[:,'isAdult'] != '1', 'isAdult'] = '0'
-    title_basics.loc[:,"isAdult"] = title_basics.loc[:,"isAdult"].map({'1': True, '0': False})
-
-    # auto convert
-    title_basics = title_basics.convert_dtypes()
+    title_basics['isAdult'] = (title_basics['isAdult']
+                            .map({1: True, 0: False})
+                            .astype(pd.ArrowDtype(pa.bool_())))
 
     # titleType
-    # There are a few null values (np.nan)
-    # convert to category
-    title_basics.loc[:,"titleType"] = title_basics.loc[:,"titleType"].astype('category')
+    title_basics["titleType"] = title_basics["titleType"].astype("category")
 
     # startYear & endYear & runtimeMinutes
-    # both contain a lot of np.nan
-    title_basics.loc[:,["startYear","endYear","runtimeMinutes"]] = title_basics.loc[:,["startYear","endYear","runtimeMinutes"]].astype('Int16')
+    title_basics[["startYear","endYear","runtimeMinutes"]
+                 ] = title_basics[["startYear","endYear","runtimeMinutes"]
+                                  ].astype(pd.ArrowDtype(pa.uint16()))
 
     # make genres table
-    genres = (title_basics
-              .genres.str.split(",", expand=True)
-              .stack()
-              .reset_index()
-              .rename(columns={"level_0":'tconst', 0:"genre"})
-              .set_index("tconst")
-              .drop(columns='level_1'))
-    genres['genre'] = genres['genre'].astype('category')
+    genres = (title_basics["genres"]
+            .dropna()
+            .str.split(",")
+            .to_frame()
+            .explode('genres')
+            .astype('category'))
 
     # drop genre
     title_basics = title_basics.drop(columns='genres')
@@ -247,38 +234,12 @@ def convertTitleBasics():
     path = os.path.join(PARQ_PATH, FILES_IMDB_PARQ['genres'])
     genres.to_parquet(path, engine='pyarrow')
 
-    # unnecessary, but i give it a try
-    del title_basics
-    del genres
-
-
-def convertTitleRate():
-    # read
-    title_rate_path = os.path.join(DOWNLOAD_PATH,FILES_IMDB['tit_rate'])
-    title_rate = pd.read_csv(title_rate_path,sep='\t', quotechar='\t', low_memory=False)
-    title_rate = title_rate.replace(to_replace = "\\N", value = np.nan)
-
-    # convert and set tconst as index
-    title_rate = setTNconst(title_rate, 'tconst', set_index=True, drop_col=True)
-
-    # convert averageRating
-    title_rate.loc[:,'averageRating'] = title_rate.loc[:,'averageRating'].astype('float32')
-
-    # convert numVotes
-    title_rate.loc[:,'numVotes'] = title_rate.loc[:,'numVotes'].astype('uint32')
-
-    # write to disk
-    path = os.path.join(PARQ_PATH, FILES_IMDB_PARQ['tit_rate'])
-    title_rate.to_parquet(path, engine='pyarrow')
-
-    # unnecessary, but i give it a try
-    del title_rate
-
 
 def convertTitleCrew():
     # read directors and writers file
     title_crew_path = os.path.join(DOWNLOAD_PATH,FILES_IMDB['cast_crew'])
-    title_crew = pd.read_csv(title_crew_path,sep='\t', quotechar='\t', low_memory=True)
+    title_crew = pd.read_csv(title_crew_path,sep='\t', quotechar='\t',
+                             low_memory=True, dtype_backend="pyarrow")
     title_crew = title_crew.replace(to_replace = "\\N", value = np.nan)
 
     # set index
@@ -286,67 +247,61 @@ def convertTitleCrew():
 
     for crew in ['directors', 'writers']:
         # make  dataframe
-        crew_frame = (title_crew.loc[:,crew]
+        crew_frame = (title_crew[crew]
                     .dropna()
                     .str.split(",")
                     .to_frame()
                     .explode(crew))
-        
+            
         # convert
         crew_frame = setTNconst(crew_frame, crew, set_index=False, drop_col=False)
-        crew_frame.loc[:,crew] = crew_frame.loc[:,crew].astype('category').to_frame()
+        crew_frame[crew] = crew_frame[crew].astype('category')
 
         # write to disk
         path = os.path.join(PARQ_PATH, FILES_IMDB_PARQ[crew])
         crew_frame.to_parquet(path, engine='pyarrow')
 
-    # unnecessary, but i give it a try
-    del title_crew
-
 
 def convertNameBasics():
     # load data
     name_bas_path = os.path.join(DOWNLOAD_PATH,FILES_IMDB['name_bas'])
-    name_bas = pd.read_csv(name_bas_path,sep='\t', low_memory=True)
-    name_bas = name_bas.replace(to_replace = "\\N", value = np.nan)
+    name_bas = pd.read_csv(name_bas_path, sep='\t', low_memory=True, dtype_backend="pyarrow")
 
     # convert
-    name_bas.loc[:,["nconst","primaryName","primaryProfession","knownForTitles"]] = name_bas.loc[:,["nconst","primaryName","primaryProfession","knownForTitles"]].astype('string')
-    name_bas.loc[:,["birthYear","deathYear"]] = name_bas.loc[:,["birthYear","deathYear"]].astype('Int16')
+    name_bas = name_bas.replace(to_replace = "\\N", value = np.nan)
+    name_bas[["birthYear","deathYear"]] = name_bas[
+        ["birthYear","deathYear"]].astype(pd.ArrowDtype(pa.uint16()))
     name_bas = setTNconst(name_bas, 'nconst', set_index=True, drop_col=True)
     name_convert = {'primaryProfession': 'prim_prof', 'knownForTitles': "known_for"}
-    
+
+    # transform to new dataframe
     for info in ['primaryProfession', 'knownForTitles']:
         # get column and delete from original
         data = name_bas[info].dropna().to_frame()
         name_bas = name_bas.drop(columns=info)
 
         # explode data
-        data = (data.loc[:,info]
-             .str.split(',')
-             .to_frame()
-             .explode(info))
+        data = (data[info]
+                .str.split(',')
+                .to_frame()
+                .explode(info))
         
         # convert
         if info == 'primaryProfession':
-            data.loc[:,info] = data.loc[:,info].astype('string').astype("category")
+            data[info] = data[info].astype("category")
         else:
             data = setTNconst(data, info, set_index=False, drop_col=False)
         
         # write to disk
         path = os.path.join(PARQ_PATH, FILES_IMDB_PARQ[name_convert[info]])
         data.to_parquet(path, engine='pyarrow')
-    
+
     # write to disk
     path = os.path.join(PARQ_PATH, FILES_IMDB_PARQ['name_bas'])
     name_bas.to_parquet(path, engine='pyarrow')
 
-    # unnecessary, but i give it a try
-    del name_bas
-
 
 def convertOrdering():
-    TEMP_START_TIME = time.time()
     # set vars
     chunk_size = int(55996061 / 3)
     tit_prin_path = os.path.join("data/imdb/download/", FILES_IMDB['tit_prin'])
@@ -354,71 +309,55 @@ def convertOrdering():
     ordering = ordering.set_index('tconst')
 
     # load file using chunks
-    for chunk in pd.read_csv(tit_prin_path,sep='\t', low_memory=True, usecols=['tconst', 'nconst', 'ordering', 'category'], chunksize=chunk_size):
+    for chunk in pd.read_csv(tit_prin_path,sep='\t', low_memory=True,
+                             usecols=['tconst', 'nconst', 'ordering', 'category'],
+                             chunksize=chunk_size, dtype_backend="pyarrow"):
         chunk = chunk.replace(to_replace = "\\N", value = np.nan)
 
         # set dtypes, convert id's and set index
-        chunk.loc[:,'ordering'] = chunk.loc[:,'ordering'].astype(int)
+        chunk['ordering'] = chunk['ordering'].astype(pd.ArrowDtype(pa.uint8()))
         chunk = setTNconst(chunk, 'tconst', set_index=True, drop_col=True)
         chunk = setTNconst(chunk, 'nconst', set_index=False, drop_col=False)
 
         # merge chunks
         ordering = pd.concat([ordering, chunk]) 
-    
-    ordering.loc[:,'category'] = ordering.loc[:,'category'].astype(str).astype('category')
+
+    ordering['category'] = ordering['category'].astype(str).astype('category')
 
     # write to disk
     path = os.path.join(PARQ_PATH, FILES_IMDB_PARQ['ordering'])
     ordering.to_parquet(path, engine='pyarrow')
 
-    # unnecessary, but i give it a try
-    del ordering
-
-    END_TIME = time.time()
-    part_time = time.strftime("%H:%M:%S", time.gmtime(END_TIME-TEMP_START_TIME))
-    total_time = time.strftime("%H:%M:%S", time.gmtime(END_TIME-START_TIME))
-    print("\tordering convert done. Ex. time: ",part_time, "\tTot. time: ",total_time)
-
 
 def convertJob():
-    TEMP_START_TIME = time.time()
     # set vars
     chunk_size = int(55996061 / 3)
     tit_prin_path = os.path.join("data/imdb/download/", FILES_IMDB['tit_prin'])
     job = pd.DataFrame(columns=['tconst', 'nconst', 'job'])
     job = job.set_index('tconst')
-
     
     # load file using chunks
-    for chunk in pd.read_csv(tit_prin_path,sep='\t', low_memory=True, usecols=['tconst', 'nconst', 'job'], chunksize=chunk_size):
+    for chunk in pd.read_csv(tit_prin_path,sep='\t', low_memory=True,
+                             usecols=['tconst', 'nconst', 'job'],
+                             chunksize=chunk_size, dtype_backend="pyarrow"):
         chunk = chunk.replace(to_replace = "\\N", value = np.nan).dropna()
 
         # set dtypes, convert id's and set index
         chunk = setTNconst(chunk, 'tconst', set_index=True, drop_col=True)
         chunk = setTNconst(chunk, 'nconst', set_index=False, drop_col=False)
-        chunk.loc[:,'job'] = chunk.loc[:,'job'].astype('category')
 
         # merge chunks
         job = pd.concat([job, chunk]) 
 
     # set job as category
-    job.loc[:,'job'] = job.loc[:,'job'].astype(str).astype('category')
+    job['job'] = job['job'].astype(str).astype('category')
 
     # write to disk
     path = os.path.join(PARQ_PATH, FILES_IMDB_PARQ['job'])
     job.to_parquet(path, engine='pyarrow')
 
-    # unnecessary, but i give it a try
-    del job
-
-    END_TIME = time.time()
-    part_time = time.strftime("%H:%M:%S", time.gmtime(END_TIME-TEMP_START_TIME))
-    total_time = time.strftime("%H:%M:%S", time.gmtime(END_TIME-START_TIME))
-    print("\tjob convert done. Ex. time: ",part_time, "\tTot. time: ",total_time)
-
 
 def convertCharacter():
-    TEMP_START_TIME = time.time()
     # set vars
     chunk_size = int(55996061 / 3)
     tit_prin_path = os.path.join("data/imdb/download/", FILES_IMDB['tit_prin'])
@@ -426,7 +365,9 @@ def convertCharacter():
     character = character.set_index('tconst')
 
     # load file using chunks
-    for chunk in pd.read_csv(tit_prin_path,sep='\t', low_memory=True, usecols=['tconst', 'nconst', 'characters'], chunksize=chunk_size):
+    for chunk in pd.read_csv(tit_prin_path, sep='\t', low_memory=True,
+                             usecols=['tconst', 'nconst', 'characters'],
+                             chunksize=chunk_size, dtype_backend="pyarrow"):
         chunk = chunk.replace(to_replace = "\\N", value = np.nan).dropna()
 
         # set dtypes, convert id's and set index
@@ -434,26 +375,17 @@ def convertCharacter():
         chunk = setTNconst(chunk, 'nconst', set_index=False, drop_col=False)
 
         # explode the list
-        chunk.loc[:,"characters"] = chunk.loc[:,"characters"].apply(eval)
+        chunk["characters"] = chunk["characters"].apply(eval)
         chunk = chunk.explode('characters')
 
         # merge chunks
         character = pd.concat([character, chunk])
 
     # set characters as catagory
-    chunk.loc[:,"characters"] = chunk.loc[:,"characters"].astype(str).astype('category')
+    character["characters"] = character["characters"].astype(str).astype('category')
 
     # write to disk
     path = os.path.join(PARQ_PATH, FILES_IMDB_PARQ['character'])
     character.to_parquet(path, engine='pyarrow')
-
-    # unnecessary, but i give it a try
-    del character
-
-    END_TIME = time.time()
-    part_time = time.strftime("%H:%M:%S", time.gmtime(END_TIME-TEMP_START_TIME))
-    total_time = time.strftime("%H:%M:%S", time.gmtime(END_TIME-START_TIME))
-    print("\tcharacter convert done. Ex. time: ",part_time, "\tTot. time: ",total_time)
-
 
 main()

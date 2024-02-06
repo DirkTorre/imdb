a548ff7f72bddae477ab1e3311f362868b4544ac
DirkTorre <dirkvdtorre@live.nl>
2024-02-06 14:37:43 +0100

    Pipeline in bash works. Added timestamps in bash pipeline file. updated todo.

--- update_all.sh
+++ update_all.sh
@@ -4,11 +4,22 @@
 # conda init bash
 # conda activate ML
 
+start=`date +%s`
+
 # downloading of imdb files
-# python download_imdb_files.py
+python download_imdb_files.py
+end=`date +%s`
+runtime=$((end-start))
+echo "download complete. runtime: ${runtime} seconds"
 
 # adding new movies to the list
 python add_to_movie_list.py
+end=`date +%s`
+runtime=$((end-start))
+echo "adding to list complete. runtime: ${runtime} seconds"
 
 # creating the movie list
 python compile_movie_list.py
+end=`date +%s`
+runtime=$((end-start))
+echo "compiling excel complete. runtime: ${runtime} seconds"
